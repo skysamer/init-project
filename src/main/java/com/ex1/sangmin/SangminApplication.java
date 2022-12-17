e828ff3a462a39eca70e1116fc74b834ba81731c
sangmin <skyslayer123@naver.com>
2022-12-17 19:38:23 +0900

    init commit

---
+++ src/main/java/com/ex1/sangmin/SangminApplication.java
@@ -0,0 +1,13 @@
+package com.ex1.sangmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SangminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SangminApplication.class, args);
+	}
+
+}
